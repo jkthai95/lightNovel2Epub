02c5e50c56a07a95ce5bb208043ed1c9490bb957
Jason <jason.thai@erdosmiller.com>
2021-08-01 23:46:46 -0500

    copy existing chapters if epub exists

--- novelUpdatesParser.py
+++ novelUpdatesParser.py
@@ -17,7 +17,9 @@ class novelUpdatesParser:
         self.downloadPathBase = downloadPathBase
         self.downloadPathSeries = None
         self.epub = None
+        self.epubOld = None
         self.chapters = []
+        self.bookPath = None
 
         if url:
             self.parseURL()
@@ -38,6 +40,15 @@ class novelUpdatesParser:
                     self.downloadPathSeries = os.path.join(self.downloadPathBase, self.seriesTitle)
                     if not os.path.exists(self.downloadPathSeries):
                         os.makedirs(self.downloadPathSeries)
+                    self.bookPath = os.path.join(self.downloadPathSeries, "{}.epub".format(self.seriesTitle))
+                    if os.path.exists(self.bookPath):
+                        renameIdx = 0
+                        renamePath = os.path.join(self.downloadPathSeries, "{} - Old {}.epub".format(self.seriesTitle, renameIdx))
+                        while os.path.exists(renamePath):
+                            renameIdx = renameIdx + 1
+                            renamePath = os.path.join(self.downloadPathSeries, "{} - Old {}.epub".format(self.seriesTitle, renameIdx))
+                        self.epubOld = epub.read_epub(self.bookPath)
+                        os.rename(self.bookPath, renamePath)
                     self.epub = epub.EpubBook()
                     self.epub.set_identifier(self.seriesTitle)
                     self.epub.set_title(self.seriesTitle)
@@ -51,27 +62,32 @@ class novelUpdatesParser:
         rows = tableBody.find_all('tr')
         for row in rows:
             cols = row.find_all('td')
-            chapter = cols[-1]
-            chapterLinks = chapter.find_all('a', href=True)
+            chapterData = cols[-1]
+            chapterLinks = chapterData.find_all('a', href=True)
             for chapterLink in chapterLinks:
                 chapter = chapterLink['title']
-                href = urlparse(chapterLink['href'], 'https').geturl()
-                self.downloadChapter(chapter, href)
+                print(chapter)
+                chapterPath = "{}.xhtml".format(chapter)
+                if self.epubOld and self.epubOld.get_item_with_href(chapterPath):
+                    print("skipping")
+                    self.copyChapter(chapter, self.epubOld.get_item_with_href(chapterPath).get_content())
+                    break
+                else:
+                    href = urlparse(chapterLink['href'], 'https').geturl()
+                    self.downloadChapter(chapter, href)
 
     def createBook(self):
         self.epub.toc = tuple(self.chapters)
 
         style = 'BODY {color: white}'
-        navCSS = epub.EpubItem(uid='style_nav', file_name='style/nav.css', media_type='test/css', content=style)
+        navCSS = epub.EpubItem(uid='style_nav', file_name='style/nav.css', media_type='text/css', content=style)
         self.epub.add_item(navCSS)
-
-        self.epub.spine = self.chapters
-
         self.epub.add_item(epub.EpubNcx())
         self.epub.add_item(epub.EpubNav())
 
-        bookPath = os.path.join(self.downloadPathSeries, "{}.epub".format(self.seriesTitle))
-        epub.write_epub(bookPath, self.epub, {})
+        self.epub.spine = self.chapters
+
+        epub.write_epub(self.bookPath, self.epub, {})
 
     def downloadChapter(self, chapter, url):
         downloadPathChapter = os.path.join(self.downloadPathSeries, chapter)
@@ -98,10 +114,13 @@ class novelUpdatesParser:
 
         self.addChapter(chapter, chapterParserMap[baseSite].text)
 
+    def copyChapter(self, chapter, html):
+        self.addChapter(chapter, html)
+
     def addChapter(self, chapter, html):
-        chapterPath = "{}.xhtml".format(os.path.join(self.downloadPathSeries, chapter))
-        chapter = epub.EpubHtml(title=chapter, file_name=chapterPath)
-        chapter.set_content(html)
-        self.epub.add_item(chapter)
-        self.chapters.append(chapter)
+        chapterPath = "{}.xhtml".format(chapter)
+        chapterItem = epub.EpubHtml(title=chapter, file_name=chapterPath, lang='en')
+        chapterItem.set_content(html)
+        self.epub.add_item(chapterItem)
+        self.chapters.insert(0, chapterItem)    # assumes chapters are inserted in reverse order
 
